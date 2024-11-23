00c277a4de133f0f9fed2c644e7fcfbe7b648155
rkdalswl718 <rkdalswl070718@gmail.com>
2024-11-24 04:50:55 +0900

    setting :: router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Splash from './pages/splash/index';
 
 function App() {
-  const location = useLocation();
   return (
-    <div>
-      <AnimatedRoutes />
-      {!noFooterPaths.includes(location.pathname) && <Footer />}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Splash} />
+      </Switch>
+    </Router>
   );
 }
 
-function AnimatedRoutes() {
-  const location = useLocation();
-  return (
-    <TransitionGroup>
-      <CSSTransition key={location.key} classNames="fade" timeout={300}>
-        <Routes location={location}>
-          <Route path="/start" element={<Start />} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
-
-function AppWithRouter() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
-
-export default AppWithRouter;
+export default App;
--- src/pages/splash/index.jsx
+++ src/pages/splash/index.jsx
@@ -1,1 +1,11 @@
-r
+import React from 'react';
+
+const Splash = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  );
+};
+
+export default Splash;
