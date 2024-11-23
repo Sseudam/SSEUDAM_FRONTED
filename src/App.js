74373cbbd3f87a846f4a4b82de08665bf4318950
rkdalswl718 <rkdalswl070718@gmail.com>
2024-11-24 08:07:35 +0900

    add :: splash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Splash from './pages/splash/index';
+import Splash from './pages/splash/index.jsx';
+import './App.css';
 
 function App() {
   return (
--- src/pages/splash/index.jsx
+++ src/pages/splash/index.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { Logo } from "../../assets/Logo";
+import React from 'react';
+import Logo from '../../assets/Logo.jsx';
 
 const Splash = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="">
+    <div className="w-full h-[100vh] pb-32 flex flex-col items-center justify-center">
+      <div className="text-[16px] text-[#9c9c9c]">분리배출의 시작, 쓰담쓰담</div>
         <Logo />
-      </div>
     </div>
   );
 };
